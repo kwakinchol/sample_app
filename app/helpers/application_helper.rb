2a0e6faf96cfb69751c30de259e4856d280ff280
James Kwak <jameskwak@Jamess-iMac.local>
2016-06-14 17:55:59 -0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def full_title(page_title=nil)
+		if page_title
+			return "#{page_title} | Ruby on Rails Tutorial Sample App"
+		else 
+			return "| Ruby Tutorial Sample App" 
+		end
+	end
 end
