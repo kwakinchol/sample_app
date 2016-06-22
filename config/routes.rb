21335ee172e95e7a8666754be3563005bcfe6712
James Kwak <jameskwak@Jamess-iMac.local>
2016-06-22 14:28:34 -0700

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
-  
 
   root 'static_pages#home'
-  #get  'static_pages/home'
-  get  'about' => 'static_pages#about'
-  get  'help' => 'static_pages#help'
-  get  'contact' => 'static_pages#contact'
-  get  'signup' => 'users#new'
+    get   'about' => 'static_pages#about'
+    get    'help' => 'static_pages#help'
+    get 'contact' => 'static_pages#contact'
+    get  'signup' => 'users#new'
+    get   'login' => 'sessions#new'
+    post  'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
 
